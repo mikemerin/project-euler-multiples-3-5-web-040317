43e8adefe98a0a7a64eb811c399162f22d21267c
Mike Merin <mikemerinweather@gmail.com>
2017-04-17 09:14:48 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,7 @@
-# Enter your procedural solution here!
+def collect_multiples(n)
+
+  (1...n).select { |x| x % 3 == 0 || x % 5 == 0 }
+
+end
+
+def sum_multiples(n) collect_multiples(n).reduce(:+) end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,11 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(n) @n = n end
+
+  def collect_multiples
+    (1...@n).select { |x| x % 3 == 0 || x % 5 == 0 }
+  end
+
+  def sum_multiples() collect_multiples.reduce(:+) end
+
+end
